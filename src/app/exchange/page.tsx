3896234e71420deb0ebbdd00f50ce207c7bbb4f6
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-01 14:55:54 +0200

    add changing order of inputs and buttons

--- src/app/exchange/page.tsx
+++ src/app/exchange/page.tsx
@@ -23,6 +23,123 @@ import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 export default function ExchangePage(props: any) {
   const [InputFrom, setInputFrom] = useState("");
   const [InputTo, setInputTo] = useState("");
+  const [orderOfInputs, setOrderOfInputs] = useState("standart");
+
+  const changeOrder = () => {
+    if (orderOfInputs === "standart") {
+      setOrderOfInputs("reverse");
+      return;
+    }
+
+    setOrderOfInputs("standart");
+    return;
+  };
+
+  const standartOrder = (
+    <Grid item md={8}>
+      <Card variant="outlined">
+        <CardContent>
+          <Grid container spacing={2} item m={1}>
+            <Grid item>
+              <Header></Header>
+            </Grid>
+
+            <Grid container item spacing={2} justifyContent={"space-between"}>
+              <Grid item md={3}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+              <Grid item md={3} justifyContent={"right"}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+            </Grid>
+
+            <Grid container item justifyContent={"space-between"}>
+              <Grid item md={4}>
+                <TextField
+                  onChange={(e) => setInputFrom(e.target.value)}
+                  value={InputFrom}
+                  id="filled-basic"
+                  label="Crypto"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item md={1} alignContent="center">
+                <IconButton onClick={changeOrder} color="primary">
+                  <CurrencyExchangeIcon></CurrencyExchangeIcon>
+                </IconButton>
+              </Grid>
+              <Grid item md={4} mr={5}>
+                <TextField
+                  onChange={(e) => setInputTo(e.target.value)}
+                  value={InputTo}
+                  label="Crypto"
+                  id="filled-basic"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid item mb={2}>
+              <Button variant="contained">Exchange</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+
+  const reverseOrder = (
+    <Grid item md={8}>
+      <Card variant="outlined">
+        <CardContent>
+          <Grid container spacing={2} item m={1}>
+            <Grid item>
+              <Header></Header>
+            </Grid>
+
+            <Grid container item spacing={2} justifyContent={"space-between"}>
+              <Grid item md={3}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+              <Grid item md={3} justifyContent={"right"}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+            </Grid>
+
+            <Grid container item justifyContent={"space-between"}>
+              <Grid item md={4}>
+                <TextField
+                  onChange={(e) => setInputTo(e.target.value)}
+                  value={InputTo}
+                  label="Crypto"
+                  id="filled-basic"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item md={1} alignContent="center">
+                <IconButton onClick={changeOrder} color="primary">
+                  <CurrencyExchangeIcon></CurrencyExchangeIcon>
+                </IconButton>
+              </Grid>
+              <Grid item md={4} mr={5}>
+                <TextField
+                  onChange={(e) => setInputFrom(e.target.value)}
+                  value={InputFrom}
+                  id="filled-basic"
+                  label="Crypto"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid item mb={2}>
+              <Button variant="contained">Exchange</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 
   return (
     <AppRouterCacheProvider>
@@ -31,62 +148,7 @@ export default function ExchangePage(props: any) {
           <Grid item md={12}>
             <TextField id="filled-basic" label="Address" variant="outlined" />
           </Grid>
-
-          <Grid item md={8}>
-            <Card variant="outlined">
-              <CardContent>
-                <Grid container spacing={2} item m={1}>
-                  <Grid item>
-                    <Header></Header>
-                  </Grid>
-
-                  <Grid
-                    container
-                    item
-                    spacing={2}
-                    justifyContent={"space-between"}
-                  >
-                    <Grid item md={3}>
-                      <CryptoButtons></CryptoButtons>
-                    </Grid>
-                    <Grid item md={3} justifyContent={"right"}>
-                      <CryptoButtons></CryptoButtons>
-                    </Grid>
-                  </Grid>
-
-                  <Grid container item justifyContent={"space-between"}>
-                    <Grid item md={4}>
-                      <TextField
-                        onChange={(e) => setInputFrom(e.target.value)}
-                        value={InputFrom}
-                        id="filled-basic"
-                        label="Filled"
-                        variant="outlined"
-                      />
-                    </Grid>
-                    <Grid item md={1} alignContent="center">
-                      <IconButton color="primary">
-                        <CurrencyExchangeIcon></CurrencyExchangeIcon>
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={4} mr={5}>
-                      <TextField
-                        onChange={(e) => setInputTo(e.target.value)}
-                        value={InputTo}
-                        label="Filled"
-                        id="filled-basic"
-                        variant="outlined"
-                      />
-                    </Grid>
-                  </Grid>
-
-                  <Grid item mb={2}>
-                    <Button variant="contained">Exchange</Button>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </Grid>
+          {orderOfInputs === "standart" ? standartOrder : reverseOrder}
         </Grid>
       </Box>
     </AppRouterCacheProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,125 @@ import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 export default function AppPage() {
   const [cryptoInput, setCryptoInput] = useState("");
   const [fiatInput, setFiatInput] = useState("");
+  const [orderOfInputs, setOrderOfInputs] = useState("standart");
+  const [selectedCrypto, setSelectedCrypto] = useState(null);
+  const [selectedFiat, setSelectedFiat] = useState(null);
+
+  const changeOrder = () => {
+    if (orderOfInputs === "standart") {
+      setOrderOfInputs("reverse");
+      return;
+    }
+
+    setOrderOfInputs("standart");
+    return;
+  };
+
+  const standartOrder = (
+    <Grid item md={8}>
+      <Card variant="outlined">
+        <CardContent>
+          <Grid container spacing={2} item m={1}>
+            <Grid item>
+              <Header></Header>
+            </Grid>
+
+            <Grid container item spacing={2} justifyContent={"space-between"}>
+              <Grid item md={3}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+              <Grid item md={3} justifyContent={"right"}>
+                <FiatButtons></FiatButtons>
+              </Grid>
+            </Grid>
+
+            <Grid container item justifyContent={"space-between"}>
+              <Grid item md={4}>
+                <TextField
+                  onChange={(e) => setCryptoInput(e.target.value)}
+                  value={cryptoInput}
+                  id="filled-basic"
+                  label="Crypto"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item md={1} alignContent="center">
+                <IconButton onClick={() => changeOrder()} color="primary">
+                  <CurrencyExchangeIcon></CurrencyExchangeIcon>
+                </IconButton>
+              </Grid>
+              <Grid item md={4} mr={5}>
+                <TextField
+                  onChange={(e) => setFiatInput(e.target.value)}
+                  value={fiatInput}
+                  label="Fiat"
+                  id="filled-basic"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid item mb={2}>
+              <Button variant="contained">Buy</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+
+  const reverseOrder = (
+    <Grid item md={8}>
+      <Card variant="outlined">
+        <CardContent>
+          <Grid container spacing={2} item m={1}>
+            <Grid item>
+              <Header></Header>
+            </Grid>
+
+            <Grid container item spacing={2} justifyContent={"space-between"}>
+              <Grid item md={3}>
+                <FiatButtons></FiatButtons>
+              </Grid>
+              <Grid item md={3} justifyContent={"right"}>
+                <CryptoButtons></CryptoButtons>
+              </Grid>
+            </Grid>
+
+            <Grid container item justifyContent={"space-between"}>
+              <Grid item md={4}>
+                <TextField
+                  onChange={(e) => setFiatInput(e.target.value)}
+                  value={fiatInput}
+                  label="Fiat"
+                  id="filled-basic"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item md={1} alignContent="center">
+                <IconButton onClick={() => changeOrder()} color="primary">
+                  <CurrencyExchangeIcon></CurrencyExchangeIcon>
+                </IconButton>
+              </Grid>
+              <Grid item md={4} mr={5}>
+                <TextField
+                  onChange={(e) => setCryptoInput(e.target.value)}
+                  value={cryptoInput}
+                  id="filled-basic"
+                  label="Crypto"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid item mb={2}>
+              <Button variant="contained">Buy</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 
   return (
     <AppRouterCacheProvider>
@@ -32,62 +151,7 @@ export default function AppPage() {
           <Grid item md={12}>
             <TextField id="filled-basic" label="Address" variant="outlined" />
           </Grid>
-
-          <Grid item md={8}>
-            <Card variant="outlined">
-              <CardContent>
-                <Grid container spacing={2} item m={1}>
-                  <Grid item>
-                    <Header></Header>
-                  </Grid>
-
-                  <Grid
-                    container
-                    item
-                    spacing={2}
-                    justifyContent={"space-between"}
-                  >
-                    <Grid item md={3}>
-                      <CryptoButtons></CryptoButtons>
-                    </Grid>
-                    <Grid item md={3} justifyContent={"right"}>
-                      <FiatButtons></FiatButtons>
-                    </Grid>
-                  </Grid>
-
-                  <Grid container item justifyContent={"space-between"}>
-                    <Grid item md={4}>
-                      <TextField
-                        onChange={(e) => setCryptoInput(e.target.value)}
-                        value={cryptoInput}
-                        id="filled-basic"
-                        label="Filled"
-                        variant="outlined"
-                      />
-                    </Grid>
-                    <Grid item md={1} alignContent="center">
-                      <IconButton color="primary">
-                        <CurrencyExchangeIcon></CurrencyExchangeIcon>
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={4} mr={5}>
-                      <TextField
-                        onChange={(e) => setFiatInput(e.target.value)}
-                        value={fiatInput}
-                        label="Filled"
-                        id="filled-basic"
-                        variant="outlined"
-                      />
-                    </Grid>
-                  </Grid>
-
-                  <Grid item mb={2}>
-                    <Button variant="contained">Buy</Button>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </Grid>
+          {orderOfInputs === "standart" ? standartOrder : reverseOrder}
         </Grid>
       </Box>
     </AppRouterCacheProvider>
