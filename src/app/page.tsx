fe37a902882e2ebc0b691403b012590a460ec2d4
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-02 21:28:02 +0200

    add changing second input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import Header from "@/UI/Header";
 import CryptoButtons from "@/UI/CryptoButtons";
 import FiatButtons from "@/UI/FiatButtons";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
@@ -22,11 +22,12 @@ import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 // or `v1X-appRouter` if you are using Next.js v1X
 
 export default function AppPage() {
-  const [cryptoInput, setCryptoInput] = useState("");
-  const [fiatInput, setFiatInput] = useState("");
+  const [cryptoInput, setCryptoInput] = useState(0);
+  const [fiatInput, setFiatInput] = useState(0);
   const [orderOfInputs, setOrderOfInputs] = useState("standart");
   const [selectedCrypto, setSelectedCrypto] = useState("BTC");
   const [selectedFiat, setSelectedFiat] = useState("USD");
+  const [pairCurrence, setPairCurrence] = useState(60000);
 
   const changeOrder = () => {
     if (orderOfInputs === "standart") {
@@ -37,6 +38,28 @@ export default function AppPage() {
     setOrderOfInputs("standart");
     return;
   };
+
+  useEffect(() => {
+    if (selectedCrypto === "BTC" && selectedFiat === "USD") {
+      setPairCurrence(60000);
+    } else if (selectedCrypto === "BTC" && selectedFiat === "EUR") {
+      setPairCurrence(50000);
+    } else if (selectedCrypto === "BTC" && selectedFiat === "RUB") {
+      setPairCurrence(6000000);
+    } else if (selectedCrypto === "USDT" && selectedFiat === "USD") {
+      setPairCurrence(1);
+    } else if (selectedCrypto === "USDT" && selectedFiat === "EUR") {
+      setPairCurrence(0.9);
+    } else if (selectedCrypto === "USDT" && selectedFiat === "RUB") {
+      setPairCurrence(90);
+    } else if (selectedCrypto === "ETH" && selectedFiat === "USD") {
+      setPairCurrence(3200);
+    } else if (selectedCrypto === "ETH" && selectedFiat === "EUR") {
+      setPairCurrence(3000);
+    } else if (selectedCrypto === "ETH" && selectedFiat === "RUB") {
+      setPairCurrence(300000);
+    }
+  }, [selectedCrypto, selectedFiat]);
 
   const standartOrder = (
     <Grid item md={8}>
@@ -65,7 +88,9 @@ export default function AppPage() {
             <Grid container item justifyContent={"space-between"}>
               <Grid item md={4}>
                 <TextField
-                  onChange={(e) => setCryptoInput(e.target.value)}
+                  onChange={(e) => {
+                    setCryptoInput(+e.target.value);
+                  }}
                   value={cryptoInput}
                   id="filled-basic"
                   label="Crypto"
@@ -73,14 +98,20 @@ export default function AppPage() {
                 />
               </Grid>
               <Grid item md={1} alignContent="center">
-                <IconButton onClick={() => changeOrder()} color="primary">
+                <IconButton
+                  onClick={() => {
+                    changeOrder();
+                    setFiatInput(cryptoInput * pairCurrence);
+                  }}
+                  color="primary"
+                >
                   <CurrencyExchangeIcon></CurrencyExchangeIcon>
                 </IconButton>
               </Grid>
               <Grid item md={4} mr={5}>
                 <TextField
-                  onChange={(e) => setFiatInput(e.target.value)}
-                  value={fiatInput}
+                  disabled
+                  value={cryptoInput * pairCurrence}
                   label="Fiat"
                   id="filled-basic"
                   variant="outlined"
@@ -124,7 +155,7 @@ export default function AppPage() {
             <Grid container item justifyContent={"space-between"}>
               <Grid item md={4}>
                 <TextField
-                  onChange={(e) => setFiatInput(e.target.value)}
+                  onChange={(e) => setFiatInput(+e.target.value)}
                   value={fiatInput}
                   label="Fiat"
                   id="filled-basic"
@@ -132,14 +163,21 @@ export default function AppPage() {
                 />
               </Grid>
               <Grid item md={1} alignContent="center">
-                <IconButton onClick={() => changeOrder()} color="primary">
+                <IconButton
+                  onClick={() => {
+                    changeOrder();
+                    setCryptoInput(fiatInput / pairCurrence);
+                  }}
+                  color="primary"
+                >
                   <CurrencyExchangeIcon></CurrencyExchangeIcon>
                 </IconButton>
               </Grid>
               <Grid item md={4} mr={5}>
                 <TextField
-                  onChange={(e) => setCryptoInput(e.target.value)}
-                  value={cryptoInput}
+                  disabled
+                  onChange={(e) => setCryptoInput(+e.target.value)}
+                  value={fiatInput / pairCurrence}
                   id="filled-basic"
                   label="Crypto"
                   variant="outlined"
